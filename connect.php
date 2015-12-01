bb620215a70abf76f85e57b4793ba60646f8bee0
lud200 <dludaya09@outlook.com>
2015-12-01 08:44:11 -0500

    columns edited

--- connect.php
+++ connect.php
@@ -8,14 +8,14 @@ if($link === false){
 }
  
 // Attempt insert query execution
-$sql = "INSERT INTO player VALUES ('1003', 'Jack', 29, 130, 23)";
+$sql = "INSERT INTO player VALUES ('1004', 'Jay', 54, 130, 23)";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
  
- $query = "select * from player";
+ $query = "SELECT PlayerID, Name, Credits, LifetimeSpins, SaltValue FROM player";
  $result = mysqli_query($link, $query);
 
  $emptyarray = array();
